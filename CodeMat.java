76277b85e7d2a554ef06642e668d74fe5ae71dab
tannmayygupta <guptatb_1@rknec.edu>
2025-04-02 21:47:20 +0530

    CodeChef Contest START180D

---
+++ CodeMat.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class CodeMat{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		
+		int Y = sc.nextInt();
+		
+		if(X >= Y){
+		    System.out.println("No");
+		}
+		else{
+		    System.out.println("Yes");
+		}
+    }
+}
