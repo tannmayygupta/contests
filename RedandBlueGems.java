4bc074eed66303369c0b5deca024cbbb665fd9bd
tannmayygupta <guptatb_1@rknec.edu>
2025-05-28 22:19:39 +0530

    CodeChef Contest START186D Q. Red and Blue Gems

---
+++ RedandBlueGems.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RedandBlueGems{
+    public static void main(String[] args) {
+        		
+		Scanner sc = new Scanner(System.in);
+		
+		int R = sc.nextInt();
+		
+		int B = sc.nextInt();
+		
+		int P = sc.nextInt();
+		
+		int Q = sc.nextInt();
+		
+		System.out.println(Math.max( R*P , B*Q ));
+        
+    }
+}
