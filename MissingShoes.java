dc44ef1085b2f3f15c79e2e12fe9c48ddf43f820
tannmayygupta <guptatb_1@rknec.edu>
2025-06-26 15:36:13 +0530

    CodeChef Contest START191D Q. Missing Shoes

---
+++ MissingShoes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// https://www.codechef.com/problems/SHOM
+public class MissingShoes {
+    public static void main(String[] args) {
+        
+        		// your code goes here  
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int L = sc.nextInt();
+		
+		int R = sc.nextInt();
+		
+		System.out.println(Math.abs(L - R));
+
+	}
+}
