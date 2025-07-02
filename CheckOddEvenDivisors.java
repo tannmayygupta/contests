b56b2132ff1dce2b30081a416c0c2f6a71c13bed
tannmayygupta <guptatb_1@rknec.edu>
2025-07-02 23:10:04 +0530

    CodeChef Contest START191D Q. Check Odd Even Divisors

---
+++ CheckOddEvenDivisors.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class CheckOddEvenDivisors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        while(t != 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            
+            if(heya(A, B)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+            
+            t--;
+        }
+    }
+
+    public static boolean heya(int A, int B) {
+        
+        if(A <= 0) {
+            return false;
+        }
+        
+        if(B == 0) {
+            return true;
+        } else {
+            return (B % A == 0);
+        }
+    }
+}
