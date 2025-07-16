81692fd788bbdb14cd3bc6a5b4417ed9d2dd546e
tannmayygupta <guptatb_1@rknec.edu>
2025-07-16 23:09:19 +0530

    CodeChef Contest START195D  Q. Selling Sandwiches

---
+++ SellingSandwiches.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SellingSandwiches {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        
+        int B = sc.nextInt();
+        
+        int C = sc.nextInt();
+        
+        
+        System.out.println(A - (B + C));
+        
+    }
+}
