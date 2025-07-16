536dcead87ade3153c2aff56c67f7ca27d220625
tannmayygupta <guptatb_1@rknec.edu>
2025-07-16 23:13:07 +0530

    CodeChef Contest START195D  Q. Marble Collector

---
+++ MarbleCollector.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MarbleCollector {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t-- != 0){
+		    int N = sc.nextInt();
+		    
+		    int M = sc.nextInt();
+		    
+		    int[] arr = new int[N];
+		    
+		    int[] M1 = new int[M + 1] ;
+		    
+		    for(int i = 0 ; i < N ; i++){
+		        arr[i] = sc.nextInt();
+		        M1[arr[i]]++;
+		    }
+		    
+		    Arrays.sort(M1);
+		    
+		    int count = 0 ;
+		    
+		    for(int j = M1.length - 1 ; j >= 1 ; j-- ){
+		        if(M1[j] > 0){
+		            count++;
+		        }
+		    }
+		    
+		    System.out.println(M - count);
+		}
+    }
+}
