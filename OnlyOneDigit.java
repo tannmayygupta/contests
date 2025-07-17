810409bbf5cbff6e06c96b31c68b1a3dfe4ca664
tannmayygupta <guptatb_1@rknec.edu>
2025-07-17 20:43:49 +0530

    CodeForces A. Only One Digit

---
+++ OnlyOneDigit.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class OnlyOneDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- != 0) {
+            int x = sc.nextInt();
+
+            System.out.println(smallestdigit(x));
+        }
+        
+    }
+
+    static int smallestdigit(int x){
+
+        int min = Integer.MAX_VALUE;
+
+        while(x > 0){
+            
+            int rem = x % 10 ;
+            min = Math.min(min, rem);
+            x = x / 10 ;
+        }
+
+        return min ;
+    }
+}
