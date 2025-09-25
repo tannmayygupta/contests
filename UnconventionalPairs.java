33aa7595c0cffd6f467c0233b8c3c3fbb1368a4a
tannmayygupta <tanmayg460@gmail.com>
2025-09-25 22:30:01 +0530

    Contest Codeforces Round 1054 (Div. 3) (B. Unconventional Pairs)

---
+++ UnconventionalPairs.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class UnconventionalPairs {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- > 0){
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = sc.nextInt();
+            }
+
+            Arrays.sort(arr);
+
+            int max = Integer.MIN_VALUE ;
+            
+            for(int i = 0 ; i < n - 1 ; i += 2){
+                int diff = arr[i] - arr[i+1];
+
+                int modul = Math.abs(diff);
+
+                if(max < modul){
+                    max = modul ;
+                }
+            }
+
+            System.out.println(max);
+
+        }
+    }
+}
