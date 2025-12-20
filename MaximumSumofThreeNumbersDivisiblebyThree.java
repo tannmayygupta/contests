b2189f9be06c13d041b29aef24c858bb043df55b
tannmayygupta <tanmayg460@gmail.com>
2025-12-20 22:28:08 +0530

    Maximum Sum of Three Numbers Divisible by Three this is optimised

--- MaximumSumofThreeNumbersDivisiblebyThree.java
+++ MaximumSumofThreeNumbersDivisiblebyThree.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
@@ -10,33 +11,56 @@ public class MaximumSumofThreeNumbersDivisiblebyThree {
 
     }
 
-    public static int minOperations(int[] nums) {
+    // this gives tle so optimized version is here down
 
-        // duplicate
+    // public static int minOperations(int[] nums) {
 
-        int i = 0;
-        int op = 0;
+    //     // duplicate
 
-        while (i < nums.length && containDuplicate(nums, i)) {
-            i += 3;
-            op++;
-        }
+    //     int i = 0;
+    //     int op = 0;
 
-        return op;
+    //     while (i < nums.length && containDuplicate(nums, i)) {
+    //         i += 3;
+    //         op++;
+    //     }
 
-    }
+    //     return op;
 
-    public static boolean containDuplicate(int[] arr, int st) {
+    // }
 
-        HashSet<Integer> ans = new HashSet<>();
+    // public static boolean containDuplicate(int[] arr, int st) {
 
-        for (int i = st; i < arr.length; i++) {
-            if (ans.contains(arr[i])) {
-                return true;
+    //     HashSet<Integer> ans = new HashSet<>();
+
+    //     for (int i = st; i < arr.length; i++) {
+    //         if (ans.contains(arr[i])) {
+    //             return true;
+    //         }
+    //         ans.add(arr[i]);
+    //     }
+
+    //     return false;
+    // }
+
+    public static int minOperations(int[] nums){
+        
+        int n = nums.length;
+        HashMap<Integer, Integer> lo = new HashMap<>();
+        int min = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (lo.containsKey(nums[i])) {
+
+                min = Math.max(min , lo.get(nums[i]) + 1);
             }
-            ans.add(arr[i]);
+            lo.put(nums[i], i);
+        }
+        
+        if (min == 0) {
+            return 0;
         }
 
-        return false;
+        return (min + 2) / 3;
     }
 }
