46a603341160f299a9db0aa3c96f37f2a976d3bf
tannmayygupta <tanmayg460@gmail.com>
2025-12-23 21:21:09 +0530

    Codeforces DIV 3 Contest

--- BlackslexandPassword.java
+++ BlackslexandPassword.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// A. Blackslex and Password
 public class BlackslexandPassword {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
