a43a20973d5c4a27dfe51882f9f28aead355be7d
tannmayygupta <tanmayg460@gmail.com>
2025-12-31 23:35:55 +0530

    Pizza Comparision

---
+++ PizzaComparision.java
@@ -0,0 +1,26 @@
+public class PizzaComparision {
+    public static void main(String[] args) {
+        // your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t--!=0 ){
+		    int A = sc.nextInt();
+		    int B = sc.nextInt();
+		    
+		    int areaA = 10 * 10 ;
+		    int areaB = 15 * 15 ;
+		    
+		    double costA = (double) areaA / A  ;
+		    double costB = (double) areaB / B;
+		    
+		    if(costA > costB){
+		        System.out.println("Small");
+		    }else if(costB > costA){
+		        System.out.println("Large");
+		    }else{
+		        System.out.println("Equal");
+		    }
+		}
+    }
+}
