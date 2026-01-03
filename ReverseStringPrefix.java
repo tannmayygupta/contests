cac46d3f6fb968e3223368a14021de067fac647d
tannmayygupta <tanmayg460@gmail.com>
2026-01-03 22:43:00 +0530

    Q1. Reverse String Prefix ©leetcode contest jan 3

---
+++ ReverseStringPrefix.java
@@ -0,0 +1,25 @@
+public class ReverseStringPrefix {
+    public static void main(String[] args) {
+        String s = "Tanmay";
+        int k = 5;
+
+        System.out.println(reversePrefix(s, k));
+    }
+
+    public static String reversePrefix(String s, int k) {
+        char[] ch = s.toCharArray();
+
+        int i = 0 ;
+        int j = k - 1; 
+
+        while(i < j){
+            char temp = ch[i];
+            ch[i] = ch[j] ;
+            ch[j] = temp ; 
+            i++;
+            j--;
+        }
+
+        return String.valueOf(ch);
+    }
+}
