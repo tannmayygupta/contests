21d8afa197539b0662b097ff139513cded1f0951
tannmayygupta <tanmayg460@gmail.com>
2026-01-07 23:41:08 +0530

    New Year Resolution codechef contest weekly

---
+++ NewYearResolution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class NewYearResolution {
+    
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n * 31 
+            
+        );
+	}
+}
