e8aa55a55b0cd66ad959714c047b9d9a51f78521
tannmayygupta <tanmayg460@gmail.com>
2026-01-12 23:25:19 +0530

    Codeforces contest div 3

--- SocialExperiment.java
+++ SocialExperiment.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
-
+ 
 public class SocialExperiment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+ 
         int t = sc.nextInt();
-
+ 
         while (t--!=0) {
             int n = sc.nextInt();
-
+ 
             int ans = Integer.MAX_VALUE ;
-
-            for(int i = 0 ; i <= n/2 ; i++){
-                for(int j = 0 ; j <= n/2 ; j++){
-                    if(2*i + 3*j == n){
-                        ans = Math.min(ans , Math.abs(2*i - 3*j));
-                    }
-                }
+ 
+            // for(int i = 0 ; i <= n/2 ; i++){
+            //     for(int j = 0 ; j <= n/2 ; j++){
+            //         if(2*i + 3*j == n){
+            //             ans = Math.min(ans , Math.abs(2*i - 3*j));
+            //         }
+            //     }
+            // }
+ 
+            if(n == 2 || n == 3){
+                System.out.println(n);
+                continue  ;
             }
-
-            System.out.println(ans);
+ 
+            if(n%2 == 0){
+                System.out.println("0");
+            }else{
+                System.out.println("1");
+            }
         }
-
     }
 }
