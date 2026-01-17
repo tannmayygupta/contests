09939d025c77207c0bbf6b634573b93c759ee839
tannmayygupta <tanmayg460@gmail.com>
2026-01-17 20:55:18 +0530

    Best Reachable Tower leetcode biweekly contest

---
+++ BestReachableTower.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BestReachableTower {
+    public static void main(String[] args) {
+        int[][] towers = { {1,2,5 } , {2,1,7} , {3,1,9} };
+        int[] center = {1,1};
+        int radius = 2 ;
+
+        System.out.println(Arrays.toString(bestTower(towers, center, radius)));
+        
+    }
+    public static int[] bestTower(int[][] towers, int[] center, int radius) {
+        int cx = center[0];
+        int cy = center[1];
+        int[] ans = {-1,-1};
+        int maxquality = Integer.MIN_VALUE ; 
+        for(int i = 0 ; i < towers.length ; i++){
+            int x = towers[i][0];
+            int y = towers[i][1];
+            int quality = towers[i][2];
+            int manhattandis =  Math.abs(x - cx) + Math.abs(y - cy);
+            // maxmanhattandis = Math.max(maxmanhattandis, manhattandis);
+            if(manhattandis <= radius){
+                if( maxquality < quality || (maxquality == quality && lexicoSmallest(x,y,ans[0],ans[1]))){
+                    ans[0] = x ;
+                    ans[1] = y ;
+                    maxquality = quality ;
+                }
+            }
+        }
+
+        return ans ;
+    }
+    public static boolean lexicoSmallest(int x , int y , int x1 , int y1){
+        if(x1 == -1){
+            return true ;
+        }
+        return (x < x1 || (x == x1  && y < y1)); 
+        // this function means to returun the smallest cordinate if x , y is sa
+        // the smallest cordinate if x , y is smaller the function return true otherwise false 
+    }
+
+}
