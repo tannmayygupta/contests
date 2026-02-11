f17af2ceff9805d3337807b44118f8d95a72089f
tannmayygupta <tanmayg460@gmail.com>
2026-02-11 22:28:38 +0530

    All Even(cc contest starter)

---
+++ AllEven.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AllEven {
+
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    
+		    int oddCou = 0 ;
+		    int evenCou = 0 ;
+		    int[] nums = new int[n];
+		    
+		    for(int i = 0 ; i < n ; i++){
+		        nums[i] = sc.nextInt();
+		    }
+		    
+		    ArrayList<Integer> arr = new ArrayList<>();
+		    
+		    for(int i = 0 ; i < n ; i++){
+		      //  nums[i] = sc.nextInt();
+		      arr.add(nums[i]);
+		    }
+		    
+		    
+		    while(arr.size() > 1){
+		        int sum = arr.get(arr.size() - 1) + arr.get(arr.size() - 2);
+		        arr.set(arr.size() - 2 , sum);
+		        arr.remove(arr.size() - 1);
+		    }
+		    
+		    if(arr.get(0) % 2 == 0){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		    
+		}
+
+	}
+
+
+}
--- MergeAdjacentEqualElements.java
+++ MergeAdjacentEqualElements.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class MergeAdjacentEqualElements {
     public static void main(String[] args) {
@@ -7,52 +8,21 @@ public class MergeAdjacentEqualElements {
         System.out.println(mergeAdjacent(nums));
         
     }
-    public static List<Long> mergeAdjacent(int[] nums) {
-
-        int n = nums.length ;
-
+    
+    public static List<Long> mergeAdjacent(int[] nums){
+        Stack<Long> stk = new Stack<>();
+        // 3 1 1 2
+        for(int i = 0 ; i < nums.length ; i++){
+            long cur = nums[i] ;
+            while (!stk.empty() && stk.peek() == cur) {
+                long sum = stk.pop() + cur ;
+                stk.push(sum);
+                break ;
+            }
+            stk.push((long) nums[i]);
+        }
         List<Long> ar = new ArrayList<>();
-
-        for(int i = 0 ; i < n ; i++){
-
-            ar.add( (long) nums[i]);
-
-        }
-
-        boolean merge = true ;
-
-        while(merge){
-
-            
-
-            merge = false ;
-
-            // [1,1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536]
-
-            for(int i = 0 ; i < ar.size() - 1; i++){
-
-                if(ar.get(i).equals(ar.get(i+1))){
-
-                    long sum = (long) ar.get(i) + ar.get(i+1);
-
-                    ar.set(i,sum);
-
-                    ar.remove(i+1);
-
-                    merge = true ;
-
-                    break;
-
-                }
-
-                
-
-            } 
-
-        }
-
         return ar ;
-
     }
 
 }
