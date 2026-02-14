256aa87e9efcd1aa84e26044b63e32d0998e4fd1
tannmayygupta <tanmayg460@gmail.com>
2026-02-14 23:16:34 +0530

    Q1. Weighted Word Mapping

--- WeightedWordMapping.java
+++ WeightedWordMapping.java
@@ -1,4 +1,6 @@
 public class WeightedWordMapping {
+    // Q1. Weighted Word Mapping
+    // https://leetcode.com/problems/weighted-word-mapping/description/
     public static void main(String[] args) {
         String[] words = {"abcd","def","xyz"};
         int[] weights = {5,3,12,14,1,2,3,2,10,6,6,9,7,8,7,10,8,9,6,9,9,8,3,7,7,2};
