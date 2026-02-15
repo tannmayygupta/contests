a9efeccfe4c6f0d6b5acebb0e133091a2a9b5291
tannmayygupta <tanmayg460@gmail.com>
2026-02-15 22:16:34 +0530

    A. Sieve of Erato67henes(cf div3)

---
+++ SieveofErato67henes.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SieveofErato67henes {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            boolean one = false;
+            boolean sixseven = false;
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                if (arr[i] == 1) {
+                    one = true;
+                }
+                if (arr[i] == 67) {
+                    sixseven = true;
+                }
+            }
+
+            if (sixseven && one) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
