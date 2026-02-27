fca76c15154a40fc79c0d39d1291809287f0e61e
tannmayygupta <tanmayg460@gmail.com>
2026-02-27 21:19:56 +0530

    C. Specialty String  (codeforces div3 27/fev contest)

---
+++ SpecialtyString.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SpecialtyString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t--!=0) {
+            int n = sc.nextInt() ;
+
+            String s = sc.next();
+            
+            Stack<Character> stk = new Stack<>();
+
+            for(int i = 0 ; i < n ; i++){
+                char ch = s.charAt(i);
+
+                if(!stk.empty() && stk.peek() == ch){
+                    stk.pop();
+                }else{
+                    stk.push(ch);
+                }
+
+            }
+
+            if(stk.empty()){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+
+            // for(int i = 0 ; i < n ; i++){
+            //     for(int j = i + 1 ; i)
+            // }
+        }
+    }
+}
