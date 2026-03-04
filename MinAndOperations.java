60f0daf8ddeadaf436c005a16f81a944e5125921
tannmayygupta <tanmayg460@gmail.com>
2026-03-05 01:57:41 +0530

    codechef q2

---
+++ MinAndOperations.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MinAndOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    	int t = sc.nextInt();
+	    while(t--!= 0){
+	        int n =sc.nextInt();
+	        int ans = 0 ;
+	        
+	        for(int i = 2 ; i <= n ;  i+=2){
+	           ans += i ;
+	        }
+	        
+	        System.out.println(ans);
+	        
+	    }
+    }
+}
