989a1a0854f1be369acfa541f6089ec87d4d561b
tannmayygupta <tanmayg460@gmail.com>
2026-03-11 22:54:22 +0530

    Sum Not Equal (codechef starter contest)

---
+++ SumNotEqual.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SumNotEqual {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    
+		    int[][] arr = new int[n][2];
+		    
+		    for(int i = 0 ; i < n ; i++){
+		        arr[i][0] = sc.nextInt();
+		        arr[i][1] = i+1;
+		    }
+		  
+		    Arrays.sort(arr, (x,y) -> x[0] - y[0]);
+		    
+		    if (arr[0][0] + arr[1][0] != arr[n - 1][0]) {
+                System.out.println(arr[0][1] + " " + arr[1][1] + " " + arr[n - 1][1]);
+            } else if (arr[n - 1][0] + arr[n - 2][0] != arr[0][0]) {
+                System.out.println(arr[n - 1][1] + " " + arr[n - 2][1] + " " + arr[0][1]);
+            } else {
+                System.out.println(-1);
+            }  
+	
+		}
+
+	}
+}
