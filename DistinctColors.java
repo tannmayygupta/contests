809b134680f7e78a83f2d7cf75dc9c1632424599
tannmayygupta <tanmayg460@gmail.com>
2026-03-11 22:55:49 +0530

    Distinct Colors

---
+++ DistinctColors.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DistinctColors {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc =new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    
+		    int[] arr = new int[n];
+		    
+		    for(int i = 0 ; i < n ; i++){
+		        arr[i] = sc.nextInt();
+		    }
+		    Arrays.sort(arr);
+		    
+		    System.out.println(arr[n-1]);
+		}
+
+	}
+}
