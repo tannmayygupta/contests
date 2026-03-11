9324750c0a31f64aa7c6944da69221514d458361
tannmayygupta <tanmayg460@gmail.com>
2026-03-11 22:57:09 +0530

    Vacation Excitement(codechef contest)

---
+++ VacationExcitement.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class VacationExcitement {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int ans = x - 1 ;
+		
+		System.out.println(y + ans);
+
+	}
+}
