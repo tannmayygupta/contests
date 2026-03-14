d25382feae891984fa6e41ead3f6f6f334edd318
tannmayygupta <tanmayg460@gmail.com>
2026-03-14 21:29:12 +0530

    Q1. First Unique Even Element(contest biweekly contest)

--- FirstUniqueEvenElement.java
+++ FirstUniqueEvenElement.java
@@ -1,5 +1,6 @@
 public class FirstUniqueEvenElement {
-
+// Q1. First Unique Even Element©leetcode
+// https://leetcode.com/contest/biweekly-contest-178/problems/first-unique-even-element/description/
     public static void main(String[] args) {
         int[] nums = {3,4,2,5,4,6};
         System.out.println(firstUniqueEven(nums));
