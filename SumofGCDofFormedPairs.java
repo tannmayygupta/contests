715e8e6608e55a44c59e7e4eb05f0987ae666772
tannmayygupta <tanmayg460@gmail.com>
2026-03-15 23:39:02 +0530

    3867. Sum of GCD of Formed Pairs(contest biweekly contest)

---
+++ SumofGCDofFormedPairs.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class SumofGCDofFormedPairs {
+    public static void main(String[] args) {
+        int[] nums = {3,6,2,8};
+        System.out.println(gcdSum(nums));
+    }
+    public static long gcdSum(int[] nums) {
+        int n = nums.length ;
+        int curmax = Integer.MIN_VALUE ;
+        int[] pr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            if(curmax < nums[i]){
+                curmax = nums[i];
+            }
+
+            pr[i] = gcd(curmax , nums[i]);
+        }
+
+        Arrays.sort(pr);
+
+        int j = 0 ;
+        int k = n - 1 ;
+        long sum = 0 ;
+        while(j < k){
+            sum += gcd(pr[j],pr[k]);
+            j++;
+            k--;
+        }
+
+        return sum ;
+    }
+
+    public int gcd(int a, int b){
+        if(b == 0){
+            return a ;
+        }
+
+        return gcd(b, a%b);
+    }
+}
