f39732028e28551eb92bde7f14194e2a1e293343
tannmayygupta <tanmayg460@gmail.com>
2026-04-09 00:36:47 +0530

    codechef contest: Movie

---
+++ Movie.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Movie {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t--!=0){
+		    int n = sc.nextInt();
+		    int m = sc.nextInt();
+		    int a = sc.nextInt();
+		    int b = sc.nextInt();
+		    int c = sc.nextInt();
+		    
+		    int mincom = Math.min(n,m);
+		    
+		    if(n > m){
+		        int rem = n - m ;
+		        int ans = c * mincom + rem * a ;
+		        System.out.println(ans);
+		    }else{
+		        int rem = m - n ;
+		        int ans = c * mincom + rem * b ;
+		        System.out.println(ans);
+		    }
+		}
+
+	}
+}
