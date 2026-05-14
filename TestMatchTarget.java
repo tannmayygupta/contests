48f8daf6a86b00d5ffc5bf1b3090a0fe184f2856
tannmayygupta <tanmayg460@gmail.com>
2026-05-15 03:20:06 +0530

    Test Match Target codechef upsolve the contest as not able to give yesterday

---
+++ TestMatchTarget.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TestMatchTarget {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc = new Scanner(System.in);
+        
+        int fstin = sc.nextInt();
+        int secin = sc.nextInt();
+        int thirin = sc.nextInt();
+        
+        int winrun = fstin + thirin - secin ;
+        
+        if(winrun < 0){
+            System.out.println("0");
+        }else{
+            System.out.println(winrun + 1);
+        }
+	}
+}
